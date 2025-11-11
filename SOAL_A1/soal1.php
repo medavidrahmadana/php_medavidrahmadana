33d04a42874858821534a2d332d5f46b0c9972cc
David Rahmadana <me.davidrahmadana@gmail.com>
2025-11-11 16:27:11 +0700

    Soal 1 (PHP)
    Sistem sudah berjalan sesuai soal

---
+++ SOAL_A1/soal1.php
@@ -0,0 +1,55 @@
+<?php
+// Soal 1 (input)
+if (!isset($_POST['step'])) {
+?>
+    <form method="POST">
+        <input type="hidden" name="step" value="2">
+        <label>Inputkan Jumlah Baris:</label>
+        <input type="number" name="baris" required> (contoh: 1)
+        <br><br>
+        <label>Inputkan Jumlah Kolom:</label>
+        <input type="number" name="kolom" required> (contoh: 3)
+        <br><br>
+        <button type="submit">SUBMIT</button>
+    </form>
+<?php
+    exit;
+}
+
+// Soal 2 (Generate Text field dan sesuai baris)
+if ($_POST['step'] == 2) {
+    $baris = $_POST['baris'];
+    $kolom = $_POST['kolom'];
+?>
+    <form method="POST">
+        <input type="hidden" name="step" value="3">
+        <input type="hidden" name="baris" value="<?= $baris ?>">
+        <input type="hidden" name="kolom" value="<?= $kolom ?>">
+
+        <?php
+        for ($i = 1; $i <= $baris; $i++) {
+            for ($j = 1; $j <= $kolom; $j++) {
+                echo "<label>$i.$j:</label> ";
+                echo "<input type='text' name='data[$i][$j]' required> &nbsp;&nbsp;";
+            }
+            echo "<br>";
+        }
+        ?>
+        <br>
+        <button type="submit">SUBMIT</button>
+    </form>
+<?php
+    exit;
+}
+
+// Soal 3 (HASIL)
+if ($_POST['step'] == 3) {
+    $data = $_POST['data'];
+
+    foreach ($data as $i => $baris) {
+        foreach ($baris as $j => $value) {
+            echo "$i.$j : $value <br>";
+        }
+    }
+}
+?>
